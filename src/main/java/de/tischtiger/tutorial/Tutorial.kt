63cbc1c7d142bab5a2a7a5ddbcb76cf04df294d2
TischTiger <tischtiger@gmail.com>
2021-07-23 12:49:57 +0200

    Folge 2

--- src/main/java/de/tischtiger/tutorial/Tutorial.kt
+++ src/main/java/de/tischtiger/tutorial/Tutorial.kt
@@ -1,16 +1,26 @@
 package de.tischtiger.tutorial
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 
 class Tutorial : JavaPlugin() {
     override fun onEnable() {
-        Bukkit.getConsoleSender().sendMessage("Hallo Welt!")
-        // Plugin startup logic
+        // Strings(Zeichenketten), var(Variablen), val(Werte/Value)
+        val name = "TischTiger"
+        var output = "Hallo \" \\ \$ \n " + name + "!"
+        val output2 = "Hallo \" \\ \$ \n ${name}!"
+        val output3 = "${ChatColor.GREEN} Dieser Text ist Grün!${ChatColor.RESET} Dieser nicht!"
+        // Hallo " \ $
+        //  Welt!
+        Bukkit.getConsoleSender().sendMessage(output)
+        Bukkit.getConsoleSender().sendMessage(output2)
+        Bukkit.getConsoleSender().sendMessage(output3)
+        output = "Hallo Welt!"
+        output = "$output!"
+        Bukkit.getConsoleSender().sendMessage(output)
     }
 
     override fun onDisable() {
-        Bukkit.getConsoleSender().sendMessage("Ciao")
-        // Plugin shutdown logic
     }
 }
