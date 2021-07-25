348e6ac71a769327ca09a8a0dc072c160fc1dfbe
TischTiger <tischtiger@gmail.com>
2021-07-25 19:08:20 +0200

    Folge 2 - Zweiter Versuch

--- src/main/java/de/tischtiger/tutorial/Tutorial.kt
+++ src/main/java/de/tischtiger/tutorial/Tutorial.kt
@@ -6,19 +6,31 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Tutorial : JavaPlugin() {
     override fun onEnable() {
-        // Strings(Zeichenketten), var(Variablen), val(Werte/Value)
-        val name = "TischTiger"
-        var output = "Hallo \" \\ \$ \n " + name + "!"
-        val output2 = "Hallo \" \\ \$ \n ${name}!"
-        val output3 = "${ChatColor.GREEN} Dieser Text ist Grün!${ChatColor.RESET} Dieser nicht!"
-        // Hallo " \ $
-        //  Welt!
-        Bukkit.getConsoleSender().sendMessage(output)
+        // Strings(Zeichenketten), val(Value, Werte), var(Variablen)
+        val output1 = "Hallo Welt!"
+        Bukkit.getConsoleSender().sendMessage(output1)
+        // Hallo Welt!
+        var output2 = "Hallo Welt!"
         Bukkit.getConsoleSender().sendMessage(output2)
+        // Hallo Welt!
+        val output3 = " \" \\ \$ \nNeue Zeile"
         Bukkit.getConsoleSender().sendMessage(output3)
-        output = "Hallo Welt!"
-        output = "$output!"
-        Bukkit.getConsoleSender().sendMessage(output)
+        // " \ $
+        // Neue Zeile
+        val output4 = output1 + " GG"
+        Bukkit.getConsoleSender().sendMessage(output4)
+        // Hallo Welt! GG
+        val vorname = "Max"
+        val nachname = "Mustermann"
+        val output5 = "Hallo $vorname ${nachname}1!"
+        Bukkit.getConsoleSender().sendMessage(output4)
+        // Hallo Max Mustermann1!
+        val output6 = "${ChatColor.GREEN}Grün§rGrau"
+        output2 = "Test"
+        Bukkit.getConsoleSender().sendMessage(output2)
+        // Test
+        Bukkit.getConsoleSender().sendMessage("output2: $output2")
+        // output2: Test
     }
 
     override fun onDisable() {
