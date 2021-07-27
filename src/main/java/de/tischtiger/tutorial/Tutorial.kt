667b12e24e0583bd99ac2e607816dfe54af91d4c
Lester Kortenhoeven <lester@kortenhoeven.de>
2021-07-27 14:27:19 +0200

    Folge 3

--- src/main/java/de/tischtiger/tutorial/Tutorial.kt
+++ src/main/java/de/tischtiger/tutorial/Tutorial.kt
@@ -4,35 +4,36 @@ import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 
+class Person {
+    var vorname = ""
+    var nachname = ""
+
+    fun vollername() : String {
+        return "$vorname $nachname"
+    }
+}
+
 class Tutorial : JavaPlugin() {
     override fun onEnable() {
-        // Strings(Zeichenketten), val(Value, Werte), var(Variablen)
-        val output1 = "Hallo Welt!"
-        Bukkit.getConsoleSender().sendMessage(output1)
-        // Hallo Welt!
-        var output2 = "Hallo Welt!"
-        Bukkit.getConsoleSender().sendMessage(output2)
-        // Hallo Welt!
-        val output3 = " \" \\ \$ \nNeue Zeile"
-        Bukkit.getConsoleSender().sendMessage(output3)
-        // " \ $
-        // Neue Zeile
-        val output4 = output1 + " GG"
-        Bukkit.getConsoleSender().sendMessage(output4)
-        // Hallo Welt! GG
-        val vorname = "Max"
-        val nachname = "Mustermann"
-        val output5 = "Hallo $vorname ${nachname}1!"
-        Bukkit.getConsoleSender().sendMessage(output4)
-        // Hallo Max Mustermann1!
-        val output6 = "${ChatColor.GREEN}Grün§rGrau"
-        output2 = "Test"
-        Bukkit.getConsoleSender().sendMessage(output2)
-        // Test
-        Bukkit.getConsoleSender().sendMessage("output2: $output2")
-        // output2: Test
+        // class(Klassen) und fun(Funktionen/Methoden)
+        output("Hallo Welt!")
+        // Tutorial: Hallo Welt!
+        val p1 = Person()
+        p1.vorname = "Eva"
+        p1.nachname = "Musterfrau"
+        val p2 = Person()
+        p2.vorname = "Max"
+        p2.nachname = "Mustermann"
+        output("${p1.vollername()} liebt ${p2.vollername()}")
+        // Tutorial: Eva Musterfrau liebt Max Mustermann
     }
 
     override fun onDisable() {
+        output("Ciao")
+        // Tutorial: Ciao
+    }
+
+    fun output(msg: String) {
+        Bukkit.getConsoleSender().sendMessage("Tutorial: $msg")
     }
 }
